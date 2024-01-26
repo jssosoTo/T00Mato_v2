6ef76b0bb917ed14547480453f487c8aae6cc00c
suyuxiang-tastien <suyuxiang@tastien.com>
2024-01-26 17:45:00 +0800

    🔧 build(全局): 起源路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    origin: 'https://t00-mato-v2-theta.vercel.app',
     proxy: {
       '/api': {
         target: 'https://bored-woolens-fly.cyclic.app/',
