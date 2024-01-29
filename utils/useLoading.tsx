e914f5a5a239a8078e3313c6bf5b69ec2b3ccbff
suyuxiang-tastien <suyuxiang@tastien.com>
2024-01-29 11:03:20 +0800

    🐞 fix(全局): useLoading解构错误的问题

--- utils/useLoading.tsx
+++ utils/useLoading.tsx
@@ -10,7 +10,7 @@ export default function useLoading(func: any) {
     setLoading(true);
     try {
       const res = await func();
-      const data = await res.json();
+      const data = res.data;
 
       if (data.code !== 0) {
         throw new Error('请求有误，请重试！');
